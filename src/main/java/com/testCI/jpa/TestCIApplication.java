dab783367c6ba54733649223da6d175e4f55fa5c
Anastasiya Piashko <anapiashko@gmail.com>
2021-06-23 17:22:00 +0300

    f push

--- src/main/java/com/testCI/jpa/TestCIApplication.java
+++ src/main/java/com/testCI/jpa/TestCIApplication.java
@@ -5,59 +5,32 @@ import com.testCI.jpa.model.Author;
 import com.testCI.jpa.model.Book;
 import com.testCI.jpa.model.Publisher;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
 
 public class TestCIApplication {
 
     public static void main(String[] args) {
 
-        Map<String, Integer> headersMap = new HashMap<>();
-        headersMap.put("ArticleNumber", 0);
-        headersMap.put("AudienceCategories", 1);
-        headersMap.put("CustomerCategories", 2);
-        headersMap.put("Details__c", 3);
-        headersMap.put("Id", 4);
-        headersMap.put("ProductCategories", 5);
-        headersMap.put("Resolution", 6);
-        headersMap.put("Summary", 7);
-        headersMap.put("Title", 8);
-        headersMap.put("UrlName", 9);
-        headersMap.put("VisiblePublicKB", 10);
-        headersMap.put("LastModifiedDate", 11);
-        headersMap.put("PublishStatus", 12);
+        AuthorRepo authorRepo = new AuthorRepoImpl();
+        BookRepo bookRepo = new BookRepoImpl();
+        PublisherRepo publisherRepo = new PublisherRepoImpl();
 
-        List<String> headers = new ArrayList<>(headersMap.keySet());
-        List<String> mandatoryFields = Arrays.asList("Id", "Title", "Details__c");
-        List<String> optionalFields = headers.stream().filter(header -> !mandatoryFields.contains(header)).collect(Collectors.toList());
-        System.out.println(optionalFields);
-        System.out.println(optionalFields);
+        Book book1 = new Book("Tyref", null, null);
+        Book book2 = new Book("Hory", null, null);
 
-        String metadataColumns = "CustomerCategories ,ProductCategories, Resolution,Summary,UrlName,VisiblePublicKB,LastModifiedDate,PublishStatus";
-        String[] metadata = metadataColumns.split(" *, *");
-        System.out.println(Arrays.toString(metadata));
-        System.out.println(Arrays.toString(metadata));
+        bookRepo.create(book1);
+        bookRepo.create(book2);
 
-//        AuthorRepo authorRepo = new AuthorRepoImpl();
-//        BookRepo bookRepo = new BookRepoImpl();
-//        PublisherRepo publisherRepo = new PublisherRepoImpl();
-//
-//        Book book1 = new Book("Tyref", null, null);
-//        Book book2 = new Book("Hory", null, null);
-//
-//        bookRepo.create(book1);
-//        bookRepo.create(book2);
-//
-//        List<Book> books = bookRepo.findAll();
-//
-//        Author author = new Author("Gabriel", "Scany",books);
-//
-//       authorRepo.create(author);
-//     //  System.out.println(authorRepo.findAll());
-//
-//       Publisher publisher = new Publisher("publisher", books);
-//
-//       publisherRepo.create(publisher);
-//        System.out.println(publisherRepo.findAllBooks());
+        List<Book> books = bookRepo.findAll();
+
+        Author author = new Author("Gabriel", "Scany",books);
+
+       authorRepo.create(author);
+     //  System.out.println(authorRepo.findAll());
+
+       Publisher publisher = new Publisher("publisher", books);
+
+       publisherRepo.create(publisher);
+        System.out.println(publisherRepo.findAllBooks());
     }
 }
